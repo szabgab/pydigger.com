491797e3bdde3b84deeb6bc1636aa0369e08ef52
Gabor Szabo <gabor@szabgab.com>
2016-03-25 13:34:11 +0300

    show the data using a dynamic Flask app

--- code_maven/website.py
+++ code_maven/website.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, redirect, abort
 import time
-#import json
+import json
 #import re
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Hello World at " + time.time().__str__()
+    with open('recent.json', 'r') as f:
+        data = json.load(f)
+
+    return render_template('main.html', data = data)
