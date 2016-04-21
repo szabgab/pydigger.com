6c7724b3e2b150ddf5ae32a661c2e3a88907fe4b
Gabor Szabo <gabor@szabgab.com>
2016-04-21 22:11:31 +0300

    more stats

--- PyDigger/website.py
+++ PyDigger/website.py
@@ -56,7 +56,9 @@ cases = {
     'has_bugtrack_url'   : { '$and' : [{ 'bugtrack_url' : { '$exists' : True } }, { 'bugtrack_url' : { '$regex': '.'} }  ] },
     'no_bugtrack_url'   : { '$or' : [{ 'bugtrack_url' : { '$exists' : False } }, { 'bugtrack_url' : None }, { 'bugtrack_url' : '' } ] },
     'has_travis' : { 'travis_ci' : True},
-    'has_coveralis' : { 'coveralis' : True}
+    'has_coveralis' : { 'coveralis' : True},
+    'has_github_no_coveralis' : { '$and' : [ { 'github' : True }, {'$or' : [ { 'coveralis' : { '$exists' : False } }, { 'coveralis' : False}] }] },
+    'no_coveralis' : {'$or' : [ { 'coveralis' : { '$exists' : False } }, { 'coveralis' : False}] },
 }
 
 
