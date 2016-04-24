c8c9da40da8192cd4a1cc9b5787715f196405a3e
Gabor Szabo <gabor@szabgab.com>
2016-04-25 00:00:46 +0300

    add editconfig

--- PyDigger/fetch.py
+++ PyDigger/fetch.py
@@ -168,6 +168,8 @@ class PyPackage(object):
                     self.entry['tox'] = True # http://codespeak.net/tox/
             if e.path == 'appveyor.yml':
                     self.entry['appveyor'] = True # https://www.appveyor.com/
+            if e.path == '.editconfig':
+                    self.entry['editconfig'] = True # http://editorconfig.org/
             for field in requirements_fields:
                 if e.path == field + '.txt':
                     self.entry[field] = []
