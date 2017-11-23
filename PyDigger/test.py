4dfede17917a58dfc538a35e3ff24cc7ec39f2f7
Gabor Szabo <gabor@szabgab.com>
2017-11-23 18:13:31 +0200

    minor test

--- PyDigger/test.py
+++ PyDigger/test.py
@@ -1,7 +1,10 @@
 import PyDigger.common
 
-#PyDigger.fetch.main()
 class TestDigger(object):
     def test_fix(self):
         assert 1 == 1
 
+    def test_common(self):
+        root = PyDigger.common.get_root()
+        source_dir = PyDigger.common.get_source_dir()
+        assert root + '/src' == source_dir
