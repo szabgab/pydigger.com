f80b61964b711fd399850605b17a3280b5f18dca
Gabor Szabo <gabor@szabgab.com>
2019-10-04 16:21:46 +0300

    create config file for testing

--- PyDigger/test.py
+++ PyDigger/test.py
@@ -1,4 +1,11 @@
 import PyDigger.common
+import os
+import sys
+import yaml
+
+root = os.path.dirname(os.path.dirname(__file__))
+sys.path.insert(0, root)
+
 
 class TestDigger(object):
     def test_fix(self):
@@ -9,8 +16,12 @@ class TestDigger(object):
         source_dir = PyDigger.common.get_source_dir()
         assert root + '/src' == source_dir
 
+# TODO: Make sure the web site can be loaded even if the configuration files are missing and there is no access to the
+# databse. Report this properly in the log or on the generate web page.
+
 class TestWeb(object):
     def setup_class(self):
+        create_config_files()
         import PyDigger.website
         self.app = PyDigger.website.app.test_client()
 
@@ -33,4 +44,14 @@ class TestWeb(object):
         #print(rv.data)
         assert b'<title>About PyDigger</title>' in rv.data
 
+def create_config_files():
+    config_file = os.path.join(root, 'config.yml')
+    if not os.path.exists(config_file):
+        config = {
+            "username": "",
+            "password": "",
+            "server": "localhost:27017"
+        }
+        with open(config_file, 'w') as outfile:
+            yaml.dump(config, outfile, default_flow_style=False)
 
