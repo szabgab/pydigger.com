6f0ba922cb06e670932765c7a0a00d1905519f7a
Gabor Szabo <gabor@szabgab.com>
2023-05-01 08:52:26 +0300

    test names; test case with trailing slash; prepare example for change

--- test_vcs.py
+++ test_vcs.py
@@ -11,6 +11,7 @@ def test_vcs(tmpdir):
     with open(config_file, 'w') as outfile:
             yaml.dump(config, outfile, default_flow_style=False)
 
+    # github plain url in home_page
     package = fetch.PyPackage("foo")
     package.entry['home_page'] = 'https://github.com/user/project'
     package.entry['version'] = '1.0'
@@ -25,6 +26,23 @@ def test_vcs(tmpdir):
         'github_project': 'project'
     }
 
+    # github plain url with trailing slash in home_page
+    package = fetch.PyPackage("foo")
+    package.entry['home_page'] = 'https://github.com/user/project/'
+    package.entry['version'] = '1.0'
+    package.extract_vcs()
+    assert package.entry == {
+        'home_page': 'https://github.com/user/project/',
+        'version': '1.0',
+        'github': True,
+        'gitlab': False,
+        'bitbucket': False,
+        'github_user': 'user',
+        'github_project': 'project'
+    }
+
+
+    # gitlab plain url + version is a string in home_page
     package = fetch.PyPackage("foo")
     package.entry['home_page'] = 'https://gitlab.com/user/project'
     package.entry['version'] = 'abc'
@@ -39,6 +57,7 @@ def test_vcs(tmpdir):
         'gitlab_project': 'project'
     }
 
+    # non-vcs URL in home_page
     package = fetch.PyPackage("foo")
     package.entry['home_page'] = 'https://pypi.org/user/project'
     package.entry['version'] = 'abc'
@@ -51,3 +70,19 @@ def test_vcs(tmpdir):
         'bitbucket': False,
     }
 
+    # github url with extra /tags in home_page
+    # package = fetch.PyPackage("foo")
+    # package.entry['home_page'] = 'https://github.com/user/project/tags'
+    # package.entry['version'] = '1.0'
+    # package.extract_vcs()
+    # assert package.entry == {
+    #     'home_page': 'https://github.com/user/project/tags',
+    #     'version': '1.0',
+    #     'github': True,
+    #     'gitlab': False,
+    #     'bitbucket': False,
+    #     'github_user': 'user',
+    #     'github_project': 'project'
+    # }
+
+ 
