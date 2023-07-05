1bf398f0e211fe9f8a62d6522de250c1d48f43ca
dennistex <dennis.groisman@dazn.com>
2023-07-05 14:19:50 +0300

    fix some flake8 errors

--- tools/convert_date.py
+++ tools/convert_date.py
@@ -10,10 +10,10 @@ client = MongoClient()
 db = client.pydigger
 all = db.packages.find()
 for p in all:
-    #print(p)
+    # print(p)
     print(p['pubDate'])
     d = datetime.strptime(p['pubDate'], "%d %b %Y %H:%M:%S %Z")
-    db.packages.update({'_id' : p['_id']}, {'$set' : {'pubDate' : d}})
-    #print(db.packages.find_one({'_id' : p['_id']}))
-    #print(d)
-#    break
+    db.packages.update({'_id': p['_id']}, {'$set': {'pubDate': d}})
+    # print(db.packages.find_one({'_id' : p['_id']}))
+    # print(d)
+    # break
