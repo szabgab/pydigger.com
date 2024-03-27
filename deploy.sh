4623aa855329efb6de4327efc929fa8d83986f15
Gabor Szabo <gabor@szabgab.com>
2024-03-27 06:58:53 +0200

    Start using 'docker compose' instead of docker-compose
    Recent upgrade left me with error when trying to redeploy:
    
    KeyError: 'ContainerConfig'
    
    See https://stackoverflow.com/a/78225629/11827
    and https://docs.docker.com/compose/migrate/

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/bash
-export PATH=$PATH:/home/gabor/docker-compose/bin
+#export PATH=$PATH:/home/gabor/docker-compose/bin
 
 if [ "$1" == "deploy" ];
 then
   echo "deploy"
   git pull
-  docker-compose build
+  docker compose build
 fi
 
 if [ "$1" == "deploy" ] || [ "$1" == "restart" ];
 then
   echo "restart"
-  docker-compose stop --time 0
-  docker-compose up --detach --time 0 --remove-orphans
+  docker compose stop --timeout 0
+  docker compose up --detach --timeout 0 --remove-orphans
   docker image prune -f
 else
   echo "Usage: $0 [deploy|restart]"
